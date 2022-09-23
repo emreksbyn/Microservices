2c2006437a35c914adb94d3ec8dfb9f31991bbfc
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-09-23 12:03:37 +0300

    download ocelet to gatwayapi

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,19 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//builder.Configuration.AddJsonFile(Path.Combine("ocelot.json"));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//builder.Services.AddOcelot(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +28,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+await app.UseOcelot();
--- Services/ContactAPI/Contact.API/Program.cs
+++ Services/ContactAPI/Contact.API/Program.cs
@@ -2,23 +2,23 @@ using Contact.API.Infrastructure.AbstractServices;
 using Contact.API.Services.ConcreteServices;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://*:9000");
 
-// Add services to the container.
+//builder.WebHost.UseUrls("http://*:9000");
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IContactService, ContactService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
